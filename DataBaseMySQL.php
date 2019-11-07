28ce84d6012d3cb3da336428d93bcb3452ae4d0f
quasar <quasar@ninja>
2019-11-07 19:40:17 +0100

    Creazione di un classe per accesso alla DB MySQL

---
+++ DataBaseMySQL.php
@@ -0,0 +1,30 @@
+<?php
+
+class DataBaseMySQL{
+    protected $connessione = null;
+    
+    
+    public function __costruct(string $nome_db,string $utente,string $password,string $host='localhost'){
+        // mi connetto al MySql Server
+        $this->connessione = mysql_connect($host, $utente, $password) or die('Errore connessione MySQL');
+        
+        mysql_select_db($nome_db, $this->connessione) or die('Errore accesso alla BD "'.$nome_db.'"');
+    }
+    
+    public function SQL($query) {
+        return mysql_query($query, $this->connessione) or die('Errore query SQL...');
+    }
+    
+    public function chiudi() {
+        if(!is_null($this->connessione)){
+            mysql_close($this->connessione);
+            $this->connessione = null;
+        }
+    }
+    
+    // distruttore
+    public function __destruct() {
+        self::chiudi();
+    }
+}
+
