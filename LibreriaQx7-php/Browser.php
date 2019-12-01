4777310e8486ff9482192d0eef0db480d1f05577
quasar <quasar@ninja>
2019-12-01 11:22:38 +0100

    aggiunta tabella info client/server

--- LibreriaQx7-php/Browser.php
+++ LibreriaQx7-php/Browser.php
@@ -73,4 +73,32 @@ class Browser {
         return $this->dati.'';
     }
     
+    static public function infoClient() {
+        
+        $dati = '';
+        $chiaveServer = array(
+            'PHP_SELF','argv','argc','GATEWAY_INTERFACE','SERVER_ADDR','SERVER_NAME',
+            'SERVER_SOFTWARE','SERVER_PROTOCOL','REQUEST_METHOD','REQUEST_TIME',
+            'REQUEST_TIME_FLOAT','QUERY_STRING','DOCUMENT_ROOT','HTTP_ACCEPT','HTTP_ACCEPT_CHARSET',
+            'HTTP_ACCEPT_ENCODING','HTTP_ACCEPT_LANGUAGE','HTTP_CONNECTION','HTTP_HOST',
+            'HTTP_REFERER','HTTP_USER_AGENT','HTTPS','REMOTE_ADDR','REMOTE_HOST','REMOTE_PORT',
+            'REMOTE_USER','REDIRECT_REMOTE_USER','SCRIPT_FILENAME','SERVER_ADMIN','SERVER_PORT',
+            'SERVER_SIGNATURE','PATH_TRANSLATED','SCRIPT_NAME','REQUEST_URI','PHP_AUTH_DIGEST',
+            'PHP_AUTH_USER','PHP_AUTH_PW','AUTH_TYPE','PATH_INFO','ORIG_PATH_INFO'
+        ) ;
+        
+        
+        $dati .= '<table cellpadding="2">' ;
+        
+        foreach ($chiaveServer as $par) {
+            if (isset($_SERVER[$par])) {
+                $dati .= '<tr><td>'.$par.'</td><td>' . $_SERVER[$par] . '</td></tr>' ;
+            }else {
+                $dati .= '<tr><td>'.$par.'</td><td>-</td></tr>' ;
+            }
+        }
+        $dati .= '</table>' ;
+        
+        return $dati;
+    }
 }
