303892871e303bf684101b56829527fad15ececc
Domenico della PERUTA <quasar@localhost.localdomain>
2020-08-08 12:08:10 -0400

    aggiustamenti

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include_once 'Struttura/MultiPagina.php';
 include_once 'Struttura/PaginaRicercaWeb.php';
 
 $pagina = MultiPagina::costruisciDaFileJSON('PagineWeb/argomenti.json');
-MultiPagina::aggiungiPaginaAlFileJSON('PagineWeb/argomenti2.json',['gatto','sotto_gatto'],'micio','PagineWeb/test/pippo.html','ciao mondo');
 $pagina->crea();
 
 ?>
